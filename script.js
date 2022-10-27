59e4af0eb5b67d7eb7e8ec54efd384c9d081b1b6
Precious Nhlapho <pnhlapho59@gmail.com>
2022-10-27 20:34:56 +0200

    Correct linter errors

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach(n => (n.
-  addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-})));
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
